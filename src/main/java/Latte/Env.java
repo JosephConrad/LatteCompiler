8aa7aaaf781b2d2e7bb031c8c2ba12c9e3221767
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-02-10 02:54:23 +0100

    Latte: Env refactor

--- src/main/java/Latte/Env.java
+++ src/main/java/Latte/Env.java
@@ -3,32 +3,27 @@ package Latte;
 import java.util.*;
 
 /**
- * Created by konrad on 01/02/15.
- * Default contructor initate completely new environment
+ * Created by konrad on 01/02/15. 
  */
 public class Env {
     public List<String> predefinedFunctions = Arrays.asList("readInt", "readString", "error", "printInt", "printString", "concatenateString");
     public Map<String, String> variableType = new HashMap<String, String>();
-    private Map<String, Integer> variableValues  = new HashMap<String, Integer>();
-    private Map<String, String> variablesDefault = new HashMap<String, String>();
-    private String currentType;
+    public Map<String, Integer> varDeclarationEnv = new HashMap<String, Integer>();
+    public Map<String, Integer> variableShifts = new HashMap<String, Integer>(); // Name, Shift
+    public Map<String, Integer> argumentsShifts = new HashMap<String, Integer>(); // Name, Shift
     public int rbp = 4;
     public int ifCounter = 1;
-    public String register = "";
     public int neg;
     public int whileCounter = 1;
-    public static Map<String, String> strings = new HashMap<String, String>();
     public int jmpExpCounter = 1;
-    public boolean addIsString = false;
     public int andExpCounter = 1;
     public int orExpCounter = 1;
-    public String funName = "";
-    public Map<String, Integer> variableShifts = new HashMap<String, Integer>(); // Name, Shift
     public int localVarShift = 8;
-    public Map<String, Integer> argumentsShifts = new HashMap<String, Integer>(); // Name, Shift
     public int ileArgumentow = 0;
-
-    public Map<String, Integer> varDeclarationEnv = new HashMap<String, Integer>();
+    public String funName = "";
+    public boolean addIsString = false;
+    
+    public static Map<String, String> strings = new HashMap<String, String>();
     public static Map<String, Boolean> functionsReturnAchievibility = new HashMap<String, Boolean>();
     public static Map<String, String> functionsReturnType = new HashMap<String, String>();
     public static Map<String, Integer> functionsArgumentsNumber = new HashMap<String, Integer>();
@@ -49,10 +44,12 @@ public class Env {
         env.argumentsShifts = deepCopy(last.argumentsShifts);
         env.variableType = deepCopy(last.variableType);
         env.varDeclarationEnv = deepCopy(last.varDeclarationEnv);
-
         return env;
     }
-    
+
+    public Env(String ident_) {
+        this.funName = ident_;
+    }
     
     public static <K, V>  Map<K, V> deepCopy(Map<K,V> map) {
         HashMap<K,V> newMap = new HashMap<K, V>();
@@ -61,31 +58,4 @@ public class Env {
         }
         return newMap;
     }
-    
-
-    public Env(String ident_) {
-        this.funName = ident_;
-    }
-
-    public void setCurrentType(String currentType) {
-        this.currentType = currentType;
-    }  
-
-
-
-    @Override
-    public String toString() {
-        return "Env{" +
-                "predefinedFunctions=" + predefinedFunctions +
-                ", variableType=" + variableType +
-                ", variableValues=" + variableValues +
-                ", variablesDefault=" + variablesDefault +
-                '}';
-    }
-
-    public void addVariable(String name, Integer type) {
-        variableType.put(name, "null");
-        variableValues.put(name, type);
-    }
-
 }
