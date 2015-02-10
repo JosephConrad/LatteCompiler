31628b31f4a8f204c79150c7d544b65c33b12aba
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-02-10 02:49:35 +0100

    Latte: all backend refactored with exprVisitor

--- src/main/java/Latte/BackendASM/ExprVisitor.java
+++ src/main/java/Latte/BackendASM/ExprVisitor.java
@@ -6,59 +6,61 @@ import Latte.Env;
 import java.util.LinkedList;
 
 /**
- * Class for expressions
- 
  * Created by konrad on 05/02/15.
  */
 
 public class ExprVisitor implements Expr.Visitor<String, LinkedList<Env>>
-{   
-    
-    public String oneArg(){
+{
+    public String oneArg() {
         return "\n\tpop rax\n";
     }
 
-    public String twoArgs(){
+    public String twoArgs() {
         String asm = "\n\tpop rbx\n";
         asm += "\tpop rax\n";
         return asm;
     }
-    
-    public String visit(Latte.Absyn.EVar p, LinkedList<Env> envs)
-    {
 
+    /*
+     * Variable expression
+     */
+    public String visit(Latte.Absyn.EVar p, LinkedList<Env> envs) {
         Env env = envs.getLast();
+        
         String argument = "";
         if (env.variableShifts.containsKey(p.ident_)) {
             argument = "[rbp-"+env.variableShifts.get(p.ident_)+"]";
         }
         if (env.argumentsShifts.containsKey(p.ident_)) {
-             argument = "[rbp+"+env.argumentsShifts.get(p.ident_)+"]";
+            argument = "[rbp+"+env.argumentsShifts.get(p.ident_)+"]";
         }
-        //String asm = oneArg();
+        
         String asm = "\tmov rax, "+ argument+"\n";
         asm += "\tpush rax\n";
         return asm;
     }
 
-
-    // Literal liczbowy
-    public String visit(Latte.Absyn.ELitInt p, LinkedList<Env> env)
-    {
+    /*
+     * Integer literal expression
+     */
+    public String visit(Latte.Absyn.ELitInt p, LinkedList<Env> env) {
         String asm = "\tmov rax, " + p.integer_+"\n";
         asm += "\tpush rax\n";
         return asm;
     }
-    
-    // True
-    public String visit(Latte.Absyn.ELitTrue p, LinkedList<Env> env)
-    {
+
+    /*
+     * True value expression
+     */
+    public String visit(Latte.Absyn.ELitTrue p, LinkedList<Env> env) {
         String asm = '\t' + "mov rax, 1\n" ;
         asm += "\tpush rax\n";
         return asm;
     }
-    
-    // Wyrazenie False
+
+    /*
+     * False value expression
+     */
     public String visit(Latte.Absyn.ELitFalse p, LinkedList<Env> env)
     {
         String asm = '\t' + "mov rax, 0\n" ;
@@ -66,15 +68,14 @@ public class ExprVisitor implements Expr.Visitor<String, LinkedList<Env>>
         return asm;
     }
 
-
-
-    // Wywolanie funkcji
-    public String visit(Latte.Absyn.EApp p, LinkedList<Env> envs)
-    {
+    /*
+     * Function application expression
+     */
+    public String visit(Latte.Absyn.EApp p, LinkedList<Env> envs) {
         Env env = envs.getLast();
-        
+
         String asm = "";
-        if (env.predefinedFunctions.contains(p.ident_)){
+        if (env.predefinedFunctions.contains(p.ident_)) {
             for (Expr expr : p.listexpr_) {
                 env.ileArgumentow++;
                 asm += expr.accept(new ExprVisitor(), envs);
@@ -86,54 +87,62 @@ public class ExprVisitor implements Expr.Visitor<String, LinkedList<Env>>
             int shift = (p.listexpr_.size())*8;
             for (Expr expr : p.listexpr_) {
                 env.ileArgumentow++;
-                asm += expr.accept(new ExprVisitor(), envs);
-                //asm += "\tpop rax\n";
-                //asm += "\tmov [rsp+"+ shift +"], rax\n";
-                //shift -= 8;
+                asm += expr.accept(new ExprVisitor(), envs); 
             }
             asm += "\tcall " + p.ident_ + "\n";
             asm += "\tadd rsp, " + shift + "\n";
         }
-
         asm += "\tpush rax\n";
-
         return asm;
     }
-    
-    // Napis - zwraca identyfikator do wypisania
-    public String visit(Latte.Absyn.EString p, LinkedList<Env> envs)
-    {
+
+    /*
+     * String expression
+     */
+    public String visit(Latte.Absyn.EString p, LinkedList<Env> envs) {
         Env env = envs.getLast();
+        
         int stringNo = envs.getLast().strings.size();
         String stringID = "STR_"+env.funName + "_" + stringNo;
+        
         Env.strings.put(stringID, p.string_);
+        
         String asm = "\tmov rax, (" + stringID + ")\n";
         asm += "\tpush rax\n";
+        
         envs.getLast().addIsString = true;
+        
         return asm;
     }
-   
-    public String visit(Latte.Absyn.Neg p, LinkedList<Env> env)
-    {
+
+    /*
+     * Negative expression
+     */
+    public String visit(Latte.Absyn.Neg p, LinkedList<Env> env) {
         String asm = p.expr_.accept(new ExprVisitor(), env);
         asm += oneArg();
         asm += "\tneg rax\n";
         asm += "\tpush rax\n";
         return asm;
     }
-
-    public String visit(Latte.Absyn.Not p, LinkedList<Env> envs)
-    {
-
+    
+    /*
+     * Negated expression
+     */
+    public String visit(Latte.Absyn.Not p, LinkedList<Env> envs) {
+        
         String asm = p.expr_.accept(new ExprVisitor(), envs);
         asm += oneArg();
         asm += "\txor rax, 1\n";
         asm += "\tpush rax\n";
         return asm;
     }
-
-    public String visit(Latte.Absyn.EMul p, LinkedList<Env> envs)
-    {
+    
+    /*
+     * Multiplication expression
+     */
+    public String visit(Latte.Absyn.EMul p, LinkedList<Env> envs) {
+        
         String asm = p.expr_1.accept(new ExprVisitor(), envs);
         asm += p.expr_2.accept(new ExprVisitor(), envs);
         asm += twoArgs();
@@ -141,33 +150,45 @@ public class ExprVisitor implements Expr.Visitor<String, LinkedList<Env>>
         asm += "\tpush rax\n";
         return asm;
     }
-    public String visit(Latte.Absyn.EAdd p, LinkedList<Env> envs)
-    {
+    
+    /*
+     * Sum expression
+     */
+    public String visit(Latte.Absyn.EAdd p, LinkedList<Env> envs) {
         String type = p.returnExprType(envs);
+        
         String asm = p.expr_1.accept(new ExprVisitor(), envs);
         asm += p.expr_2.accept(new ExprVisitor(), envs);
         asm += twoArgs();
+        
         if (type == "string")
             envs.getLast().addIsString = true;
+        
         asm += p.addop_.accept(new AddOpVisitor(), envs);
+        
         envs.getLast().addIsString = false;
+        
         asm += "\tpush rax\n";
         return asm;
     }
-
-    public String visit(Latte.Absyn.ERel p, LinkedList<Env> envs)
-    {   
+    
+    /*
+     * Relation expression
+     */
+    public String visit(Latte.Absyn.ERel p, LinkedList<Env> envs) {
         Env env = envs.getLast();
+        
         String asm = p.expr_1.accept(new ExprVisitor(), envs);
         asm += p.expr_2.accept(new ExprVisitor(), envs);
-        
+
         String no = env.funName+"_"+envs.getLast().jmpExpCounter++;
+        
         asm += twoArgs();
 
         asm += '\t'+ "cmp rax, rbx\n";
         asm += p.relop_.accept(new RelOpVisitor(), envs) + " REL_TRUE_"+no + "\n";
         asm += "\tjmp REL_FALSE_"+no + "\n";
-        
+
         asm += "\nREL_TRUE_"+no+":\n";
         asm += "\tmov rax, 1\n";
         asm += "\tjmp REL_FINISH_"+no+"\n";
@@ -175,25 +196,28 @@ public class ExprVisitor implements Expr.Visitor<String, LinkedList<Env>>
         asm += "\nREL_FALSE_"+no+":\n";
         asm += "\tmov rax, 0\n";
         asm += "\nREL_FINISH_"+no+":\n";
-        
-        asm += "\tpush rax\n";
 
+        asm += "\tpush rax\n";
         return asm;
     }
-    public String visit(Latte.Absyn.EAnd p, LinkedList<Env> envs)
-    {
+
+    /*
+     * Conjunction expression
+     */
+    public String visit(Latte.Absyn.EAnd p, LinkedList<Env> envs) {
         Env env = envs.getLast();
-        
+
         String asm = p.expr_1.accept(new ExprVisitor(), envs);
 
         String no = env.funName+"_"+envs.getLast().andExpCounter++;
+        
         String label = "AFTER_AND_"+no;
         asm += "\tmov rax, [rsp]\n";
         asm += "\tcmp rax, 0\n";
-        asm += "\t je " + label + "\n";
+        asm += "\tje "+label+"\n";
 
         asm += p.expr_2.accept(new ExprVisitor(), envs);
-       
+
         asm += twoArgs();
         asm += "\tand rax, rbx\n";
         asm += "\tpush rax\n";
@@ -201,13 +225,15 @@ public class ExprVisitor implements Expr.Visitor<String, LinkedList<Env>>
         return asm;
     }
 
-
-    public String visit(Latte.Absyn.EOr p, LinkedList<Env> envs)
-    {
+    /*
+     * Alternative expression
+     */
+    public String visit(Latte.Absyn.EOr p, LinkedList<Env> envs) {
         Env env = envs.getLast();
-        
+
         String asm = p.expr_1.accept(new ExprVisitor(), envs);
         String no = env.funName+"_"+envs.getLast().orExpCounter++;
+        
         String label = "AFTER_OR_"+no;
         asm += "\tmov rax, [rsp]\n";
         asm += "\tcmp rax, 1\n";
--- src/main/java/Latte/BackendASM/ItemVisitor.java
+++ src/main/java/Latte/BackendASM/ItemVisitor.java
@@ -15,36 +15,36 @@ public class ItemVisitor implements Item.Visitor<String, LinkedList<Env>>
      */
     public String visit(Latte.Absyn.NoInit p, LinkedList<Env> envs) {
         Env env = envs.getLast();
-        
+
         String asm = "";
 
-        asm += "\tmov qword [rbp-"+env.localVarShift+"], qword 0\n"; 
-        
+        asm += "\tmov qword [rbp-"+env.localVarShift+"], qword 0\n";
+
         env.variableShifts.put(p.ident_, env.localVarShift);
         env.varDeclarationEnv.put(p.ident_, envs.size());
         env.localVarShift += 8;
-        
+
         return asm;
     }
-    
+
     /*
      *  Item with initialization
      */
     public String visit(Latte.Absyn.Init p, LinkedList<Env> envs) {
-        Env env = envs.getLast(); 
-        
+        Env env = envs.getLast();
+
         String asm = "";
-        
+
         asm += p.expr_.accept(new ExprVisitor(), envs);
-        
+
         asm += "\tpop rax\n";
         asm += "\tmov [rbp-"+env.localVarShift+"], rax\n";
-        
+
         env.variableShifts.put(p.ident_, env.localVarShift);
         env.varDeclarationEnv.put(p.ident_, envs.size());
-        env.localVarShift += 8; 
+        env.localVarShift += 8;
 
         return asm;
     }
-    
+
 }
